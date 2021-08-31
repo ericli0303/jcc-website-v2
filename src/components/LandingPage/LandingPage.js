94a4a22b66d8add4d71f2b39fdc9121b72984208
ericli0303 <ericli0303@gmail.com>
2021-08-31 02:42:05 -0400

    cleaned up some code

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,91 +1,87 @@
-import React from 'react';
-import Box from '@material-ui/core/Box';
-import {isDay} from './../Utils/time-helper';
-import './Landingpage.styles.css'
+import React, { useEffect, useState } from "react";
+import Box from "@material-ui/core/Box";
+import { isDay } from "./../Utils/time-helper";
+import "./Landingpage.styles.css";
 // import Mailchimp from './../MailPage/MailchimpForm';
 
-class LandingPage extends React.Component {
-    constructor(props) {
-        super(props);
+const LandingPage = () => {
+	const images = [
+		"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/db9abol-4c3a8d76-9a7a-4850-8448-5dd2dd1c2f32.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZGI5YWJvbC00YzNhOGQ3Ni05YTdhLTQ4NTAtODQ0OC01ZGQyZGQxYzJmMzIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TJKF4suYIgwR4gsq_B5RqdgKZYKNk3Pkrvg8DHbAkwA",
+		"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/daj4ocm-f915657c-797c-40ee-b7a5-23dd7750457c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZGFqNG9jbS1mOTE1NjU3Yy03OTdjLTQwZWUtYjdhNS0yM2RkNzc1MDQ1N2MuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PaAp8z14lGYJi8O6vtI_u7ZCcET_ThsIniENsVrZZuU",
+		"https://cdnb.artstation.com/p/assets/images/images/034/631/465/4k/jaume-rovira-llorca-d2.jpg?1612808303",
+	];
 
-        const images = [
-            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/db9abol-4c3a8d76-9a7a-4850-8448-5dd2dd1c2f32.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZGI5YWJvbC00YzNhOGQ3Ni05YTdhLTQ4NTAtODQ0OC01ZGQyZGQxYzJmMzIuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.TJKF4suYIgwR4gsq_B5RqdgKZYKNk3Pkrvg8DHbAkwA",
-            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/549e9b77-4c90-4c7f-8d0e-772a4ba70576/daj4ocm-f915657c-797c-40ee-b7a5-23dd7750457c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzU0OWU5Yjc3LTRjOTAtNGM3Zi04ZDBlLTc3MmE0YmE3MDU3NlwvZGFqNG9jbS1mOTE1NjU3Yy03OTdjLTQwZWUtYjdhNS0yM2RkNzc1MDQ1N2MuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PaAp8z14lGYJi8O6vtI_u7ZCcET_ThsIniENsVrZZuU",
-            "https://cdnb.artstation.com/p/assets/images/images/034/631/465/4k/jaume-rovira-llorca-d2.jpg?1612808303"
-        ];
+	const [image, setImage] = useState(0);
 
-        this.state = {
-            images,
-                currentImg: 0
-        }
-    }
+	useEffect(() => {
+		// 1: day
+		// 2: night
+		let newCurrentImg = 0;
 
-    componentDidMount() {
-        this.changeBackgroundImage();
-    }
+		if (isDay()) {
+			newCurrentImg = 1;
+		} else {
+			newCurrentImg = 2;
+		}
 
+		setImage(newCurrentImg);
+	}, [image, setImage]);
 
-    changeBackgroundImage() {
-        // 1: day
-        // 2: night
-        let newCurrentImg = 0;
+	const imgUrl = `url('${images[image]}')`;
+	const headerImg = {
+		backgroundImage: imgUrl,
+	};
 
-        if (isDay()) {
-            newCurrentImg = 1;
-        } else {
-            newCurrentImg = 2;
-        }
-
-        this.setState({ currentImg: newCurrentImg });
-
-    }
-
-    render() {
-        const { images, currentImg } = this.state;
-        const imgUrl = `url('${images[currentImg]}')`;
-        const headerImg = { 
-            backgroundImage: imgUrl,
-        };
-        // const gradient = {
-
-        // };
-
-        return (
-            <div>
-                {/* <div className="gradient"></div> */}
-                <div className="App-header" style={headerImg}>
-                </div>
-                <Box display="block" mx="auto" width={0.8}>
-                    <Box display="flex" flexDirection="row" flexWrap="wrap" m={5}>
-                        <img 
-                            alt='first image landing' 
-                            src="https://i.pinimg.com/736x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg"/>
-                        <Box mx={5} width={0.4}>
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </Box>
-                    </Box>
-                    <Box display="flex" flexDirection="row" flexWrap="wrap"  m={5}>
-                        <Box mx={5} width={0.4}>
-                            <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </p>
-                        </Box>
-                        <img 
-                            alt="second image landing" 
-                            src="https://i.redd.it/02g1pfot5ds41.jpg" />
-                    </Box>
-                </Box>
-            </div>
-        );
-    }
-}
+	return (
+		<div>
+			{/* <div className="gradient"></div> */}
+			<div className="App-header" style={headerImg}></div>
+			<Box display="block" mx="auto" width={0.8}>
+				<Box display="flex" flexDirection="row" flexWrap="wrap" m={5}>
+					<img
+						alt="first image landing"
+						src="https://i.pinimg.com/736x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg"
+					/>
+					<Box mx={5} width={0.4}>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore
+							eu fugiat nulla pariatur. Excepteur sint occaecat
+							cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</p>
+					</Box>
+				</Box>
+				<Box display="flex" flexDirection="row" flexWrap="wrap" m={5}>
+					<Box mx={5} width={0.4}>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing
+							elit, sed do eiusmod tempor incididunt ut labore et
+							dolore magna aliqua. Ut enim ad minim veniam, quis
+							nostrud exercitation ullamco laboris nisi ut aliquip
+							ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore
+							eu fugiat nulla pariatur. Excepteur sint occaecat
+							cupidatat non proident, sunt in culpa qui officia
+							deserunt mollit anim id est laborum.
+						</p>
+					</Box>
+					<img
+						alt="second image landing"
+						src="https://i.redd.it/02g1pfot5ds41.jpg"
+					/>
+				</Box>
+				<img
+					alt="second image landing"
+					src="https://i.redd.it/02g1pfot5ds41.jpg"
+				/>
+			</Box>
+		</div>
+	);
+};
 
 export default LandingPage;
