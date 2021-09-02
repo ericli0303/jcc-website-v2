ec5c1472ac37a8398be45f75c42c67f49fd2d6d5
saul reyna <saul.reyna90@gmail.com>
2021-09-01 22:53:27 -0400

    improvedd mailing page

--- src/components/MailPage/SimpleForm.js
+++ src/components/MailPage/SimpleForm.js
@@ -2,41 +2,70 @@ import {React, useState} from 'react';
 import './SimpleForm.styles.css'
 
 const SimpleForm = ({ status, message, className, style, onSubmitted}) => {
+    const [email, setEmail] = useState("");
+    const [invalidEmail, setInvalid] = useState(false);
     let input;
 
-    const submit = () => 
-        input &&
-        input.value.indexOf("@") > -1 &&
-        onSubmitted({
-        EMAIL: input.value
-    });
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g.test(email)) {
+            setInvalid(false);
+            console.log(email)
+            onSubmitted({
+                EMAIL: email
+            });
+        }
+        else  {
+            setInvalid(true);
+        }
+
+        setEmail("");
+    }
 
     return (
-        <div>
-            <h2 style={{color: "white"}}>Join our mailing list!</h2>
+        <div className="content__item" style={{width: "auto"}}>
+            <h2 className='text' id="mailing">Join our mailing list!</h2>
             <div className={className} style={style}>
-                {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
-                {status === "error" && (
-                    <div
-                    style={{ color: "red" }}
-                    dangerouslySetInnerHTML={{ __html: message }}
-                    />
-                )}
-                {status === "success" && (
-                    <div
-                    style={{ color: "green" }}
-                    dangerouslySetInnerHTML={{ __html: message }}
-                    />
-                )}  
-            <div className="field">
-                <input
-                    ref={node => (input = node)}
-                    type="email"
-                    placeholder="Your email"
-                />
-                </div>
-                <button className="button button--mimas" onClick={submit}><span>Submit</span></button>
+                
+                <form>
+                    <div className="field">
+                        <input
+                            ref={node => (input = node)}
+                            type="email"
+                            placeholder="Your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <button className="mailingButton mailingButton--pan" 
+                        onClick={handleSubmit}>
+                        <span>Submit</span>
+                    </button> 
+                    {status === "error" && (
+                        <div
+                        className="text mailingSubmitMessage"
+                        style={{ color: "red"  }}
+                        dangerouslySetInnerHTML={{ __html: "You're already subscribed." }}
+                        />
+            )}
+                    {status === "success" && (
+                        <div
+                        className="text mailingSubmitMessage"
+                        style={{ color: "green" }}
+                        dangerouslySetInnerHTML={{ __html: "Thank you for subscribing" }}
+                        />
+                    )}  
+                    {invalidEmail && (
+                        <div
+                        className="text mailingSubmitMessage"
+                        style={{ color: "red" }}
+                        dangerouslySetInnerHTML={{ __html: "Invalid email" }}
+                        />
+                    )}  
+                </form>  
             </div>
+            
         </div>
       );
 }
