2faccfd952d4058ece4eccc84a232cbdfde70bca
amywhying <amy23643718@gmail.com>
2021-09-06 05:41:32 -0500

    Removed unnecessary styling change

--- src/components/Navbar/MenuItem.js
+++ src/components/Navbar/MenuItem.js
@@ -1,88 +1,83 @@
 import * as React from "react";
 import { motion } from "framer-motion";
-import HomeIcon from "@material-ui/icons/Home";
-import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
-import EmailIcon from "@material-ui/icons/Email";
-import EventIcon from "@material-ui/icons/Event";
-import PaymentIcon from "@material-ui/icons/Payment";
-import history from "../history";
-import { isDay } from "./../Utils/time-helper";
+import HomeIcon from '@material-ui/icons/Home';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import EmailIcon from '@material-ui/icons/Email';
+import EventIcon from '@material-ui/icons/Event';
+import PaymentIcon from '@material-ui/icons/Payment';
+import history from '../history';
+import {isDay} from './../Utils/time-helper';
 
 const variants = {
-	open: {
-		y: 0,
-		opacity: 1,
-		transition: {
-			y: { stiffness: 1000, velocity: -100 },
-		},
-	},
-	closed: {
-		y: 50,
-		opacity: 0,
-		transition: {
-			y: { stiffness: 1000 },
-		},
-	},
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 }
+    }
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 }
+    }
+  }
 };
 
-const icons = [
-	<HomeIcon />,
-	<PhotoLibraryIcon />,
-	<EmailIcon />,
-	<EventIcon />,
-	<PaymentIcon />,
-];
-const titles = [
-	"Home",
-	"Photo Gallery",
-	"Mailing List",
-	"Events Calendar",
-	"Donations",
-];
+const icons = [<HomeIcon/>, <PhotoLibraryIcon/>, <EmailIcon/>, <EventIcon/>, <PaymentIcon/>]
+const titles = ['Home', 'Photo Gallery', 'Mailing List', 'Events Calendar', 'Donations']
 
 const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
-const enabled = [true, false, true, false, true];
-export const link = ["/", "/photo", "/mail", "/events", "/donate"];
+const enabled = [true, false, true, false, true]
+export const link = ["/", "/photo", "/mail", "/events", "/donate"]
+
 
 export const MenuItem = ({ i, toggle }) => {
-	const style = {
-		// border: `2px solid ${colors[i]}`,
-		color: enabled[i] ? (isDay() ? "black" : "seashell") : "grey",
-	};
+  const style = { 
+    // border: `2px solid ${colors[i]}`,
+    color: enabled[i] ? isDay() ? 'black' : 'seashell' : 'grey'
+  };
 
-	function handleClick(i) {
-		if (enabled[i]) {
-			history.push(link[i]);
-			toggle();
-		}
-	}
+  function handleClick(i) {
+    if (enabled[i]){
+      history.push(link[i]);
+      toggle();
+    }
+  }
 
-	function whileHover() {
-		if (enabled[i]) {
-			return { scale: 1.1 };
-		}
-	}
+  function whileHover() {
+    if (enabled[i]){
+      return {scale: 1.1}
+    }
+  }
 
-	function whileTap() {
-		if (enabled[i]) {
-			return { scale: 0.95 };
-		}
-	}
+  function whileTap() {
+    if (enabled[i]){
+      return {scale: 0.95}
+    }
+  }
 
-	return (
-		<motion.li
-			variants={variants}
-			whileHover={() => whileHover(i)}
-			whileTap={() => whileTap(i)}
-			onClick={() => handleClick(i)}
-		>
-			<div className="icon-placeholder" style={style}>
-				<div className="icon-location">{icons[i]}</div>
-			</div>
-			<div className="text-placeholder" style={style}>
-				{titles[i]}
-			</div>
-			<div className="enabled">{enabled[i]}</div>
-		</motion.li>
-	);
+  return (
+    <motion.li
+      variants={variants}
+      whileHover={() => whileHover(i)}
+      whileTap={() => whileTap(i)}
+      onClick={() => handleClick(i)} 
+    >
+        <div className="icon-placeholder" style={style} >
+              <div className="icon-location">
+                  {icons[i]}
+              </div>
+          </div>
+          <div className="text-placeholder" style={style} >
+              {titles[i]}
+          </div>
+          <div className="enabled" >
+            {enabled[i]}
+          </div>
+
+        
+    </motion.li>
+  );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,54 +4,50 @@ import { motion, useCycle } from "framer-motion";
 import { useDimensions } from "./use-dimensions";
 import { MenuToggle } from "./MenuToggle";
 import { Navigation } from "./Navigation";
-import "./Navbar.styles.css";
-import { isDay } from "./../Utils/time-helper";
+import './Navbar.styles.css'
+import {isDay} from './../Utils/time-helper';
 
 const sidebar = {
-	open: (height = 1000) => ({
-		clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-		transition: {
-			type: "spring",
-			stiffness: 20,
-			restDelta: 2,
-		},
-	}),
-	closed: {
-		clipPath: "circle(30px at 40px 40px)",
-		transition: {
-			delay: 0.5,
-			type: "spring",
-			stiffness: 400,
-			damping: 40,
-		},
-	},
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2
+    }
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40
+    }
+  }
 };
 
 const backgroundStyle = {
-	backgroundColor: isDay() ? "#E7C9A9" : "#105d81",
+  backgroundColor: isDay() ? '#E7C9A9' : '#105d81'
 };
 
 const Example = () => {
-	const [isOpen, toggleOpen] = useCycle(false, true);
-	const containerRef = useRef(null);
-	const { height } = useDimensions(containerRef);
+  const [isOpen, toggleOpen] = useCycle(false, true);
+  const containerRef = useRef(null);
+  const { height } = useDimensions(containerRef);
 
-	return (
-		<motion.nav
-			initial={false}
-			animate={isOpen ? "open" : "closed"}
-			custom={height}
-			ref={containerRef}
-		>
-			<motion.div
-				className="background"
-				style={backgroundStyle}
-				variants={sidebar}
-			/>
-			<Navigation toggle={() => toggleOpen()} />
-			<MenuToggle toggle={() => toggleOpen()} />
-		</motion.nav>
-	);
+  return (
+      <motion.nav
+        initial={false}
+        animate={isOpen ? "open" : "closed"}
+        custom={height}
+        ref={containerRef}
+      >
+        <motion.div className="background" style={backgroundStyle} variants={sidebar} />
+        <Navigation toggle={() => toggleOpen()} />
+        <MenuToggle toggle={() => toggleOpen()} />
+      </motion.nav>
+  );
 };
 
 export default Example;
