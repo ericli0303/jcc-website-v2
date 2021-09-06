2376f2dd0e090abc53d72bb9f9e4322dc0a4dd35
yvesshum <yvesshum1210@gmail.com>
2021-09-06 03:22:44 -0500

    made the icons work

--- src/components/LandingPage/LandingFooter/LandingFooter.js
+++ src/components/LandingPage/LandingFooter/LandingFooter.js
@@ -8,10 +8,10 @@ import "./socialMediaIcons.css";
 const LandingFooter = () => {
 	const socialMediaItems = [
 		{
-			title: "Instagram",
 			icon: (
 				<div
 					className="iconContainer"
+					onMouseUp={() => window.open("https://www.instagram.com/neujcc")}
 					style={{
 						backgroundImage:
 							"linear-gradient(to bottom left, #C32AA3, #4C5FD7, #7232BD, #F46F30, #FFDC7D)",
@@ -20,12 +20,11 @@ const LandingFooter = () => {
 					<InstagramIcon className="icon" fill="white" />
 				</div>
 			),
-			url: "https://www.instagram.com/neujcc/",
 		},
 		{
-			title: "Facebook",
 			icon: (
 				<div
+					onMouseUp={() => window.open("https://www.instagram.com/neujcc/")}
 					className="iconContainer"
 					style={{
 						backgroundColor: "#1877f2",
@@ -34,12 +33,11 @@ const LandingFooter = () => {
 					<FacebookIcon className="icon" fill="white" />
 				</div>
 			),
-			url: "https://www.instagram.com/neujcc/",
 		},
 		{
-			title: "Discord",
 			icon: (
 				<div
+					onMouseUp={() => window.open("https://discord.gg/RVkhqma")}
 					className="iconContainer"
 					style={{
 						backgroundColor: "#7289DA",
@@ -48,28 +46,24 @@ const LandingFooter = () => {
 					<DiscordIcon className="icon" fill="white" />
 				</div>
 			),
-			url: "https://discord.gg/RVkhqma",
 		},
 	];
 
 	return (
-		<p>
-			<div style={{ textAlign: "center" }}>
-				<StyledTitleText>JOIN US!</StyledTitleText>
-				<StyledText>Find us on social media</StyledText>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						justifyContent: "center",
-						marginTop: "2rem",
-						marginBottom: "2rem",
-					}}
-				>
-					{socialMediaItems.map((item) => item.icon)}
-				</div>
+		<div style={{ textAlign: "center", paddingBottom: "5rem" }}>
+			<StyledTitleText>JOIN US!</StyledTitleText>
+			<StyledText>Find us on social media</StyledText>
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					justifyContent: "center",
+					paddingTop: "3rem",
+				}}
+			>
+				{socialMediaItems.map((item) => item.icon)}
 			</div>
-		</p>
+		</div>
 	);
 };
 
